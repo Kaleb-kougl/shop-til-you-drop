ddf4131f9babdac46dad1ef80b0c4a856a64b9a2
Ry <ry.e.desoto@gmail.com>
2019-01-02 11:59:58 -0600

    added cart and demo sequelize table info

---
+++ app/models/cart.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+    const Cart = sequelize.define('Cart', {
+        id: {
+            type: DataTypes.INTEGER
+        },
+        item: {
+            type: DataTypes.STRING
+        },
+        price: {
+            type: DataTypes.FLOAT
+        },
+        quantity: {
+            type: DataTypes.INTEGER
+        },
+        username: {
+            type: DataTypes.STRING
+        },
+        cartname: {
+            type: DataTypes.STRING
+        },
+        shopper: {
+            type: DataTypes.STRING
+        },
+        pending: {
+            type: DataTypes.BOOLEAN
+        }
+    });
+    return Cart;
+};
---
+++ app/models/demo.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+    const Demo = sequelize.define('Demo', {
+        id: {
+            type: DataTypes.INTEGER
+        },
+        phone: {
+            type: DataTypes.STRING
+        },
+        address: {
+            type: DataTypes.STRING
+        }
+    });
+    return Demo;
+};
