fc1fded959a9178a9e111113493e8ce96a7e533e
Kaleb Kougl <kalebkougl@gmail.com>
2019-01-02 11:48:34 -0600

    adding route server response

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('./app/config/passport');
 const db = require('./app/models');
-const app = express(); 
+const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.urlencoded({ extended: true }));
@@ -17,6 +17,10 @@ require("./app/controller/routes/api-routes.js")(app);
 require("./app/controller/routes/html-routes.js")(app);
 
 db.sequelize.sync({ force: true }).then(() => {
+    app.get("/", function (req, res) {
+        res.json({ 'hello': 'world' });
+    });
+
     app.listen(PORT, () => {
         console.log("App listening on PORT " + PORT);
     })
