bc40f84ed84425cbfba226b3d28182d833267bcb
John Eastwood <jjge732@gmail.com>
2019-01-02 22:59:21 -0600

    connected search results from back-end axios call to front-end

--- app/controller/routes/api-routes.js
+++ app/controller/routes/api-routes.js
@@ -3,15 +3,25 @@ const axios = require('axios');
 
 module.exports = app => {
     app.get("/api/items/:item", (req, res) => {
+        const query = req.params.item;
         axios({
             method: "GET",
             url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/site/search?",
             params: {
-              query: req.params.item
+              query: query
             },
             headers: { "X-RapidAPI-Key": process.env.SPOONACULAR_API_KEY }
-        }).then(result => {
-            res.send(result.data.Recipes);
+        }).then(results => {
+            let data = [];
+            for (let i = 0; i < results.data.Recipes.length; i++) {
+                data[i] = {
+                    name: results.data.Recipes[i].name,
+                    cost: results.data.Recipes[i].dataPoints[0].value,
+                    imageUrl: results.data.Recipes[i].image
+                }
+            }
+            app.locals[query] = data;
+            res.redirect(`/searchResults/${req.params.item}`);
         });
     })
     app.get("/api/orders", (req, res) => {
