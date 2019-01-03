dbefde58b75031d72f5c734a18401be02dccfad0
Kaleb Kougl <kalebkougl@gmail.com>
2019-01-03 14:33:20 -0600

    making route to login, hooking up js on login, linking index to login pg

---
+++ app/views/javascript/login.js
@@ -0,0 +1,8 @@
+$('#login-btn').on('click', function (event) {
+  event.preventDefault();
+  console.log('click');
+  // $.post('/api/signup', {
+  // }).then(function (data) {
+  //   console.log(data);
+  // });
+});
