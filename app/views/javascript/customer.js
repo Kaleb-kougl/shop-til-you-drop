98d260bd8c77cc58ffccdf5770ce504144393e06
Victoria <Victoria@Victorias-MacBook-Pro.local>
2019-01-04 11:47:49 -0600

    create customer page

---
+++ app/views/javascript/customer.js
@@ -0,0 +1,13 @@
+// $('#textarea1').val('New Text');
+// M.textareaAutoResize($('#textarea1'));
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     var elems = document.querySelectorAll('.tooltipped');
+//     var instances = M.Tooltip.init(elems, options);
+// });
+
+// Or with jQuery
+
+$(document).ready(function () {
+    $('.tooltipped').tooltip();
+});
