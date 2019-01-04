0c38ac4598d24b56b440ee726ff986ae3d317bb1
John Eastwood <jjge732@gmail.com>
2019-01-04 12:38:37 -0600

    ban functionality

--- app/controller/routes/api-routes/admin.js
+++ app/controller/routes/api-routes/admin.js
@@ -1,21 +1,40 @@
 module.exports = (app, db) => {
-    // admin disable abilities
-    app.post("/api/admin/disable", (req, res) => {
+    // disable users
+    app.post('/api/admin/banned/', (req, res) => {
         db.user.update({
             activeUser: false
         }, {
             where: {
                 email: req.body.user
             }
+        }).then(banned => {
+            res.json(banned);
+        }).catch(err => {
+            console.log(err);
         })
     });
 
-    
-    app.get("/api/admin/users", (req, res) => {
+    // display all users 
+    app.get('/api/admin/users/', (req, res) => {
         db.user.findAll().then(users => {
             res.json(users);
+        }).catch(err => {
+            console.log(err);
         })
     });
 
-    
+    // unban users
+    app.delete('/api/admin/banned/', (req, res) => {
+        db.user.update({
+            activeUser: true
+        }, {
+            where: {
+                email: req.body.user
+            }
+        }).then(unbannedUser => {
+            res.json(unbannedUser);
+        }).catch(err => {
+            console.log(err);
+        })
+    })
 }
