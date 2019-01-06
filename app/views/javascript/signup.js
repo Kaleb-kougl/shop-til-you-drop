727ceaeca93f1d34c367634a577f7ed4b2984db8
Ry <ry.e.desoto@gmail.com>
2019-01-06 16:32:01 -0600

    updated signup functionality

--- app/views/javascript/signup.js
+++ app/views/javascript/signup.js
@@ -1,15 +1,31 @@
-$('#signup').on('submit', function (event) {
+$('#signupform').on('submit', function(event) {
     event.preventDefault();
-    var first_name = $('#first_name').val().trim();
-    var last_name = $('#last_name').val().trim();
-    var password = $('#password-input').val().trim();
-    var email = $('#email-input').val().trim();
-    var phone = $('#phone-input').val().trim();
-    var address = $('#address-input').val().trim();
-    var picture = $('#picture-link').val().trim();
-    var role = $('#user-role').val().trim()
-
-
+    var first_name = $('#first_name')
+        .val()
+        .trim();
+    var last_name = $('#last_name')
+        .val()
+        .trim();
+    var password = $('#password-input')
+        .val()
+        .trim();
+    var email = $('#email-input')
+        .val()
+        .trim();
+    var phone = parseInt(
+        $('#phone-input')
+            .val()
+            .trim()
+    );
+    var address = $('#address-input')
+        .val()
+        .trim();
+    var picture = $('#picture-link')
+        .val()
+        .trim();
+    var role = $('#user-role')
+        .val()
+        .trim();
 
     // 0. check for text
     if (!email || !password) {
@@ -20,7 +36,7 @@ $('#signup').on('submit', function (event) {
     // 1. check for duplication
     $.post('/api/check', {
         email: email
-    }).then(function (data) {
+    }).then(function(data) {
         // 2. if user isn't in the db, add them
         if (data == null) {
             $.post('/api/signup', {
@@ -33,7 +49,7 @@ $('#signup').on('submit', function (event) {
                 address: address,
                 picture: picture,
                 activeuser: true
-            }).then(function (data) {
+            }).then(function(data) {
                 // sequelize does the email validation - if this error comes in, show it
                 if (data.errors[0].message === 'Validation isEmail on email failed') {
                     alert('Please enter a valid email!');
