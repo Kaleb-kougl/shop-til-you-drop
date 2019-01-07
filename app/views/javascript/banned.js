dcd26b71624977bbc5307dfd5ea0ad479d5bd8f1
Ry <ry.e.desoto@gmail.com>
2019-01-07 15:37:19 -0600

    cleanup
    deleted some comments

--- app/views/javascript/banned.js
+++ app/views/javascript/banned.js
@@ -15,12 +15,6 @@ $('#bannedform').on('submit', function(event) {
 
     var subject = 'Banned User Request';
     var body_message = message + '&#13;&#10;' + 'From ' + email;
-    // '%0D%0D%0D%0DThank you ' +
-    // name +
-    // ' for submitting this error to us. Please tell us in the space above, what you were doing when the error occurred.%0D%0DReferring Page: ' +
-    // daReferrer +
-    // ' %0D%0DException Error Message:%0D-------------------------------------------%0D' +
-    // errorMsg;
 
     var mailto_link =
         'mailto:ry.e.desoto@gmail.com' + '?subject=' + subject + '&body=' + body_message;
