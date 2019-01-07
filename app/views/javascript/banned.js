21d999cda8ca4fe6bdb03026f9fab4451a2ddc44
Ry <ry.e.desoto@gmail.com>
2019-01-07 15:20:15 -0600

    banned user and customer updates
    added banned user stuff in actual materialize, updated some styling, added mailto

--- app/views/javascript/banned.js
+++ app/views/javascript/banned.js
@@ -1,4 +1,30 @@
-$('#submitmsg').on('click', function(event) {
+$('#bannedform').on('submit', function(event) {
     event.preventDefault();
-    alert('Thanks for your message');
+
+    var email = $('#email-input')
+        .val()
+        .trim();
+    var message = $('#message-input')
+        .val()
+        .trim();
+
+    console.log(email);
+    console.log(message);
+
+    // var daReferrer = document.referrer;
+
+    var subject = 'Banned User Request';
+    var body_message = message + '&#13;&#10;' + 'From ' + email;
+    // '%0D%0D%0D%0DThank you ' +
+    // name +
+    // ' for submitting this error to us. Please tell us in the space above, what you were doing when the error occurred.%0D%0DReferring Page: ' +
+    // daReferrer +
+    // ' %0D%0DException Error Message:%0D-------------------------------------------%0D' +
+    // errorMsg;
+
+    var mailto_link =
+        'mailto:ry.e.desoto@gmail.com' + '?subject=' + subject + '&body=' + body_message;
+
+    win = window.open(mailto_link, 'emailWindow');
+    if (win && win.open && !win.closed) win.close();
 });
