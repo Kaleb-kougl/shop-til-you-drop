ce6676af995d5eba0b1eb122ae7d0a0e7f8f47aa
John Eastwood <jjge732@gmail.com>
2019-01-06 22:42:33 -0600

    connected admin page to back end

--- app/views/javascript/admin.js
+++ app/views/javascript/admin.js
@@ -1,24 +1,57 @@
-var userList;
 $.ajax({
     method: 'GET',
     url: '/api/admin/users'
 }).then(res => {
     console.log(res)
-    for (var i = 0; i < res.length; i++) {
-        var userRow = $('<tr>');
+    for (let i = 0; i < res.length; i++) {
+        let status = '';
+        let toggleBan;
+        if (res[i].activeUser) {
+            status = 'active';
+            toggleBan = $('<td class="ban">').attr('data-email', res[i].email).text('Ban');
+        } else {
+            status = 'banned';
+            toggleBan = $('<td class="unban">').attr('data-email', res[i].email).text('Unban');
+        }
+        let userRow = $('<tr>');
         userRow.attr('data-email', res[i].email);
-        var userEmail = $('<td>').text(res[i].email);
-        var userRole = $('<td>').text(res[i].role);
-        var accountStatus = $('<td>').text(res[i].activeUser);
+        let userEmail = $('<td>').text(res[i].email);
+        let userRole = $('<td>').text(res[i].role);
+        let accountStatus = $('<td>').text(status);
         userRow.append(userEmail);
         userRow.append(userRole);
         userRow.append(accountStatus);
-        console.log(userRow);
+        userRow.append(toggleBan)
         $("#table-body").append(userRow)
     }
-})
+});
 
+$(document).on('click', '.ban', function() {
+    $.ajax({
+        method: 'POST',
+        url: '/api/admin/banned',
+        data: {
+            user: $(this).attr('data-email')
+        }
+    }).then(res => {
+        window.location.reload();
+        console.log(res);
+    }).catch(err => {
+        console.log(err);
+    })
+});
 
-
-
-
+$(document).on('click', '.unban', function() {
+    $.ajax({
+        method: 'DELETE',
+        url: '/api/admin/banned',
+        data: {
+            user: $(this).attr('data-email')
+        }
+    }).then(res => {
+        window.location.reload();
+        console.log(res);
+    }).catch(err => {
+        console.log(err);
+    })
+});
