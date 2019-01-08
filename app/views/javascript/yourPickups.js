fcfffe9ef8c3ccb589d0c0025025576459401159
Kaleb Kougl <kalebkougl@gmail.com>
2019-01-07 19:20:22 -0600

    rendering carousel slides, adding blue colors.

--- app/views/javascript/yourPickups.js
+++ app/views/javascript/yourPickups.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
   $.get("/api/getUser", (data) => postData(data));
 
+  var carouselColors = ['light-blue lighten-4',
+    'light-blue lighten-3', 'light-blue lighten-2',
+    'light-blue lighten-1', 'light-blue', 'light-blue darken-1',
+    'light-blue darken-2', 'light-blue darken-3',
+    'light-blue darken-4'];
+
   // get data where shopper is this person
   function postData(data) {
     let user = data.user;
@@ -9,10 +15,47 @@ $(document).ready(function () {
       url: "/api/findMyPickups",
       data: { 'user': user },
     }).done(res => {
-      // window.location.replace('/yourPickups');
-      console.log(res);
+      let data = {};
+      res.map(i => {
+        if (data[i.orderNumber] === undefined) {
+          data[i.orderNumber] = [i]
+        } else {
+          data[i.orderNumber].push(i);
+        }
+      });
+
+      // Make a new card for carousel for each order
+      let colorIndex = 0;
+      for (let order in data) {
+        colorIndex++;
+        // create newDiv for each data Point
+        let newDiv = $("<div>");
+        newDiv.addClass(`carousel-item ${carouselColors[(colorIndex % carouselColors.length)]} white-text`);
+        newDiv.attr("href", '#no');
+        // add orderNumber for lookup later
+        newDiv.attr("data-orderNumber", order[0].orderNumber);
+        // Give header
+        let newHeader = $("<h2>")
+        newHeader.html(order[0].orderNumber);
+        newDiv.append(newHeader);
+        // create list of items to purchase
+        let newUl = $("<ul>");
+        data[order].map(item => {
+          let newLi = $("<li>");
+          newLi.html(`${item.item} : ${item.quantity}`);
+          newUl.append(newLi);
+        });
+        newDiv.append(newUl);
+        $(".carousel").append(newDiv);
+      }
+      // initialize carousel so it moves
+      var instance = M.Carousel.init({
+        fullWidth: true,
+        indicators: true
+      });
+      var slider = $('.carousel');
+      slider.carousel();
     });
-    console.log(data);
   }
 
 });
