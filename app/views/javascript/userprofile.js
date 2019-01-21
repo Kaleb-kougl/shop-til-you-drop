1e74f62639c34519adba59b8d95b2711ca570bd3
Kaleb Kougl <kalebkougl@gmail.com>
2019-01-21 10:12:16 -0600

    refactor and adding comments

--- app/views/javascript/userprofile.js
+++ app/views/javascript/userprofile.js
@@ -1,6 +1,11 @@
 // renders pizza loader
 // Borrowed from Patrick Stillhart
 // https://codepen.io/arcs/pen/pbPkPL
+/**
+ * This function will take an angle in degrees and convert it to radians
+ * @param {number} deg - This should be abstracted into a module
+ * @returns {number} - This should be the degrees in radians
+ */
 let toRadians = (deg) => deg * Math.PI / 180;
 let map = (val, a1, a2, b1, b2) => b1 + (val - a1) * (b2 - b1) / (a2 - a1);
 
@@ -133,8 +138,68 @@ let pizza = new Pizza('pizza');
 
 }())
 
-$(document).ready(() => {
+/**
+ * This function will take an address, phone number, first name, and last name.
+ * It will make an HTTP request to update these fields in the database for the current user.
+ * @param {string} address - This should represents the user's address
+ * @param {string} phoneNum - This should represent the user's phone number
+ * @param {string} firstName - This should represent the user's first name
+ * @param {string} lastName - This should represent the user's last name
+ * @returns {object} - This is the result of the ajax request. 
+ */
+async function updateProfile(address, phoneNum, firstName, lastName) {
+    let result = await $.ajax({
+        type: 'PUT',
+        url: '/api/user/info/',
+        data: {
+            address: address,
+            phone: phoneNum,
+            firstName: firstName,
+            lastName: lastName
+        }
+    });
+    return result;
+}
 
+/**
+ * This function will make a get request to grab the user profile data.
+ * @returns {object} - This is the response from the api endpoint that fetches user profile data
+ */
+async function getProfile() {
+    let profileData = await $.ajax({
+        type: 'GET',
+        url: '/api/user/info/'
+    });
+    // console.log(profileData[0].Demo.firstName);
+    return profileData[0];
+}
+
+/**
+ * This function will call the getProfile function and apply the results to the page.
+ * @returns {void} - No return value
+ */
+async function updatePage() {
+    let data = await getProfile();
+    const { Demo: { imageUrl, firstName, lastName, phone, address }, email } = data;
+    $('#profile-pic').html($(`<img alt="profile-pic" src="${imageUrl}">`));
+    $('#first-name').html(`Currently, your first name is ${firstName}`);
+    $('#last-name').html(`Currently, your last name is ${lastName}`);
+    $('#username').html(`Currently, your username is ${email}`);
+    $('#phone').html(`Currently, your phone number is ${phone}`);
+    $('#address').html(`Currently, your address is ${address}`);
+    $('#pizza-container').remove();
+}
+
+/**
+ * This function will take a number of textareas and clear all of them. 
+ * @param  {...object} textareas - This will be a plethora of DOM elements that are textareas
+ */
+function clearTextareas(...textareas) {
+    textareas.forEach(textarea => textarea.val(''));
+}
+
+// When the document has loaded, run the callback
+$(document).ready(() => {
     $.ajax({
         type: 'GET',
         url: '/api/user/info/',
@@ -143,39 +208,75 @@ $(document).ready(() => {
                 alert('Please log in for access!');
                 window.location.replace('/login/');
             } else {
-                $('#profile-pic').html($(`<img alt="profile-pic" src="${info[0].Demo.imageUrl}">`));
-                $('#first-name').html(`Currently, your first name is ${info[0].Demo.firstName}`);
-                $('#last-name').html(`Currently, your last name is ${info[0].Demo.lastName}`);
-                $('#username').html(`Currently, your username is ${info[0].email}`);
-                $('#phone').html(`Currently, your phone number is ${info[0].Demo.phone}`);
-                $('#address').html(`Currently, your address is ${info[0].Demo.address}`);
-                $('#pizza-container').remove();
+                updatePage();
             }
         }
     });
 
+    /**
+     * This function will take a string and 
+     * @param {string} destination - This is a string that represents what url the user would like to access
+     */
+    function changeLocation(destination) {
+        location.assign(destination);
+    }
+
+    /**
+     * This function will take a string and remove spaces, dashes, and () then add '+' to the front.
+     * @param {string} phoneNumber - This is a string of numbers entered by the user
+     * @returns {string} - The reformated string without spaces, dashes, and (). 
+     */
+    function formatPhoneNumber(phoneNumber) {
+        let formattedPhoneNumber = phoneNumber.replace(/\D/g, '');
+        formattedPhoneNumber = formattedPhoneNumber.replace(/-/g, '');
+        formattedPhoneNumber = formattedPhoneNumber.replace(/\s/g, '');
+        return '+' + formattedPhoneNumber;
+    }
+
+    /**
+     * This is an event listener that will execute it's cb when the user clicks on 
+     * the DOM element with the id 'customer-home'
+     */
     $(document).on('click', '#customer-home', () => {
-        location.replace('/customer/')
+        changeLocation('/customer/');
     });
 
+    /**
+     * This is an event listenter for the DOM element with the id 'profile' 
+     * which will call the changeLocation fucntion
+     */
     $(document).on('click', '#profile', () => {
-        location.replace('/userprofile/')
+        changeLocation('/userprofile/');
     });
 
-    $(document).on('click', '#update', () => {
-        $.ajax({
-            type: 'PUT',
-            url: '/api/user/info/',
-            data: {
-                address: $('.address-input').val(),
-                phone: $('.phone-input').val(),
-                firstName: $('.first-name-input').val(),
-                lastName: $('.last-name-input').val()
-            },
-            success: info => {
-                console.log(info);
+    // These need some sort of validation for security purposes. 
+    // We should add the ability to change your picture b/c sometimes you hate your profile pic. 
+    $(document).on('click', '#update', async () => {
+        let addressInput = $('.address-input');
+        let phoneInput = $('.phone-input');
+        let firstNameInput = $('.first-name-input');
+        let lastNameInput = $('.last-name-input');
+        let formattedPhoneNumber;
+
+        if (phoneInput.val() !== '') {
+            formattedPhoneNumber = formatPhoneNumber(phoneInput.val());
+            // Check phone number for valid length
+            if (formattedPhoneNumber.length != 12) {
+                alert('Please insert a valid phone number');
+                return;
             }
-        });
-    })
+        } else {
+            formattedPhoneNumber = phoneInput.val().trim();
+        }
+        // if input is empty string '', then that portion will not be updated
+        updateProfile(
+            addressInput.val().trim(),
+            formattedPhoneNumber,
+            firstNameInput.val().trim(),
+            lastNameInput.val().trim())
+            .then(updatePage())
+            .catch(err => console.error(err));
+        clearTextareas(addressInput, phoneInput, firstNameInput, lastNameInput);
+    });
 
 });
